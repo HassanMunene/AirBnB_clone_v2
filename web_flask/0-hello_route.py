962eff58fcc8ea052ec08f8bafe3c072f0ffa265
HassanMunene <awanzihassan@gmail.com>
2023-07-21 17:24:42 +0300

    created my first applicationgit add .

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+start a web application that print "Hello HBNB!"
+The application should listen from all network interfaces
+on port 5000
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    will handle the root URL
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
