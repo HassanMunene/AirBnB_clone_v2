9d6cecf5922556aaadc65802d9fb78f5e819f4df
HassanMunene <awanzihassan@gmail.com>
2023-07-21 17:55:32 +0300

    pycodestyle correction

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,10 +7,11 @@ on '0.0.0.0' on port 5000
 from flask import Flask
 
 
-#initiate an app instance from Flask
+# initiate an app instance from Flask
 app = Flask(__name__)
 
-#define the root url using the route decorator
+
+# define the root url using the route decorator
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
@@ -18,13 +19,15 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
-#define /hbnb route
+
+# define /hbnb route
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """
     handle /hbnb url
     """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
