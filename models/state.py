f010f92197d70a4cd76c9eb00d4e22713a35c296
HassanMunene <awanzihassan@gmail.com>
2024-07-23 21:30:38 +0300

    refactored the code massively

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import os
+from sqlalchemy.orm import relationship
+from models.city import City
+
+storeType = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column('name', String(128), nullable=False)
+    cities = relationship(
+        'City',
+        backref='state',
+        cascade='all, delete, delete-orphan',
+        passive_deletes=True
+    )
+
+    @property
+    def cities(self):
+        """cities filestorage getter function"""
+        import models
+        cityObjects = []
+        results = models.storage.all('City').values()
+        for c in results:
+            if c.state_id == self.id:
+                cityObjects.append(c)
+        return cityObjects
